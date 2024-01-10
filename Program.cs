4ccafb2a5389cc7426a32313433c647b878b61d5
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:44:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.If_Statements_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two integer to compare");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The max number between "+num1 + " and " + num2 +" Is  " + GetMax(num1, num2));
+            Console.Read();
+
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            } else
+            {
+                return num2;
+            }
+        }
+    }
+}
